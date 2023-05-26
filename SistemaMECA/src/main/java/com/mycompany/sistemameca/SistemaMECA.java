320c794fa6a5c16b1bb6696b6dcb4237c578bfa3
KeranLuciel <202014048@uns.edu.pe>
2023-05-26 18:39:31 -0500

    borrar en main

--- SistemaMECA/src/main/java/com/mycompany/sistemameca/SistemaMECA.java
+++ SistemaMECA/src/main/java/com/mycompany/sistemameca/SistemaMECA.java
@@ -21,22 +21,6 @@ public class SistemaMECA {
     
     public static void main(String[] args) {
         System.out.println("SISTEMA MECA");
-        
-        // Crear instancias de Cliente, Vehiculo y Problema
-//    Cliente cliente = new Cliente(8548, "Yolanda", "Flores", "Av. Peru 258");
-//    Vehiculo vehiculo = new Vehiculo("Corolla", "Toyota", "Rojo", "ABC123");
-//    List<Problema> problemas = new ArrayList<>();
-//    Problema problema1 = new Problema("2023-05-23", "Problema en el motor");
-    
-    // Crear instancia de Tarjeta
-//    Tarjeta tarjeta = new Tarjeta(cliente, vehiculo, problemas);
-//        Tarjeta tarjeta = new Tarjeta(cliente, vehiculo, problema);
-//        tarjeta.crearTarjeta(8548, "Yolanda", "Flores", 
-//                "Av. Peru 258", "Corolla", "Toyota", "Rojo", "ABC123", problemas);
-//        System.out.println(tarjeta.toString());
-//        System.out.println(vehiculo.toString());
-//        System.out.println(problemas.toString());
-
         Sistema sistema = new Sistema();
         List<Problema> problemas = new ArrayList<>();
         Tarjeta tarjeta = sistema.crearTarjeta(8548, "Yolanda", "Flores", 
