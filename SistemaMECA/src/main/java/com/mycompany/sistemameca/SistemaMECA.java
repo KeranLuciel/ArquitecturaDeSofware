29a09a75e138b1c0a4ba6dd3b33aa9276b4ced8f
KeranLuciel <202014048@uns.edu.pe>
2023-05-26 22:20:39 -0500

    se agrego System.out.println("---------------------------------------------------------");

--- SistemaMECA/src/main/java/com/mycompany/sistemameca/SistemaMECA.java
+++ SistemaMECA/src/main/java/com/mycompany/sistemameca/SistemaMECA.java
@@ -21,24 +21,27 @@ public class SistemaMECA {
     
     public static void main(String[] args) {
         System.out.println("SISTEMA MECA");
-        
         Sistema sistema = new Sistema();
         Tarjeta tarjeta = sistema.crearTarjeta(8548, "Yolanda", "Flores", 
                 "Av. Peru 123", "Corrolla", "Toyota", "Rojo", "154A", "14-10-2022", "Problema en el motor");
-        
         Cliente cliente = tarjeta.getCliente();
         Reparacion reparacion = new Reparacion("15-10-2022", "Reemplazo del motor", cliente);
+        System.out.println("---------------------------------------------------------");
         
         List<Pieza> piezasNecesarias = new ArrayList<>();
         Pieza pieza1 = new Pieza("Motor", 12345, "Fabricante1", 1);
         Pieza pieza2 = new Pieza("Batería", 67890, "Fabricante2", 2);
+        System.out.println("---------------------------------------------------------");
+        
         piezasNecesarias.add(pieza1);
         piezasNecesarias.add(pieza2);
         tarjeta.setPiezasNecesarias(piezasNecesarias);
         System.out.println(tarjeta.toStringWithPiezas());
+        System.out.println("---------------------------------------------------------");
         
         Pedido pedido = new Pedido(1, "14-10-2022", reparacion, piezasNecesarias);
         System.out.println(pedido.toString());
+        System.out.println("---------------------------------------------------------");
         
         Contabilidad contabilidad = new Contabilidad(new ArrayList<>());
         Factura factura1 = new Factura(1234, "02-11-2022", tarjeta.getCliente(),100.0, contabilidad);
@@ -46,11 +49,13 @@ public class SistemaMECA {
         
         System.out.println(factura1.toString());
         System.out.println(factura2.toString());
+        System.out.println("---------------------------------------------------------");
         
         double montoTotal = contabilidad.calcularMontoTotal();
         System.out.println("\nMonto total: " + montoTotal);
-        
+        System.out.println("---------------------------------------------------------");
         String conta = contabilidad.Contabilidades((int) montoTotal);
         System.out.println("\nListado brindado por Contabilidad:" + conta);
+        System.out.println("---------------------------------------------------------");
     }
 }
