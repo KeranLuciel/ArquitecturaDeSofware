4b8c8e88e4153a66b74c71c51f10ad4284134854
KeranLuciel <202014048@uns.edu.pe>
2023-05-26 19:27:03 -0500

    contabilidad, factura editadas

--- SistemaMECA/src/main/java/com/mycompany/sistemameca/Reparacion.java
+++ SistemaMECA/src/main/java/com/mycompany/sistemameca/Reparacion.java
@@ -2,4 +2,39 @@ package com.mycompany.sistemameca;
 
 public class Reparacion {
     
+    private String fecha;
+    private String descripcion;
+    private Cliente cliente;
+    
+    public Reparacion(String fecha, String descripcion, Cliente cliente){
+        this.fecha = fecha;
+        this.descripcion = descripcion;
+        this.cliente = cliente;
+    }
+    
+    public String getFecha(){
+        return fecha;
+    } 
+    
+    public void setFecha(String fecha){
+        this.fecha = fecha;
+    }
+    
+    public String getDescripción(){
+        return descripcion;
+    } 
+    
+    public void setDescripcion(String descripcion){
+        this.descripcion = descripcion;
+    }
+    
+    public Cliente getCliente(){
+        return cliente;
+    }
+    
+    @Override
+    public String toString(){
+        return "\nReparación: " + "\n- Fecha = " + fecha + "\n- Descripción = " + descripcion +
+                "\n- Cliente = " + cliente.toString();
+    }
 }
